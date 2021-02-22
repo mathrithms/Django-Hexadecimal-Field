82417897063b5dbf3b6e9cbfcdfaa21d95336ac9
Soumya <saumyaaaaa0206@gmail.com>
2021-02-22 15:11:32 +0530

    'updated'

--- project/project/settings.py
+++ project/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w*e)4ddq(+m16dw4#(l8&+y&d4+!bb-f(%xxo!!a6j5kr!_zw*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hexadecimal_field',
 ]
 
 MIDDLEWARE = [
