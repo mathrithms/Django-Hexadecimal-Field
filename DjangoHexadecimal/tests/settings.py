12f63ec7201f05c6fa09f9e7e6f69c00a66b2f37
RidhimaKohli <ridhimakohli2001@gmail.com>
2021-02-28 18:19:49 +0530

    update

--- DjangoHexadecimal/tests/settings.py
+++ DjangoHexadecimal/tests/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
+from secret import *
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's^s^=8_4%c$s%(r3#m3$46e(t!$ruo-j#kgndq@!haa52%_v*3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
