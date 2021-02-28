ded5c0bd60a1f8722d0aeaafb51859a9488b19cf
RidhimaKohli <ridhimakohli2001@gmail.com>
2021-02-28 19:31:23 +0530

    update

--- DjangoHexadecimal/tests/settings.py
+++ DjangoHexadecimal/tests/settings.py
@@ -11,14 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from .secret import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-from secret import *
+
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-   
+
 }
 
 
