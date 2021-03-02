5d2f5b838e999e9bc24e64d00aecf35207d0d2da
Nivedit Jain <niveditjain@outlook.com>
2021-03-02 23:02:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
 from setuptools import setup
 
 setup(
-    name='DjangoHexadecimalField',
+    name="DjangoHexadecimalField",
+    version="0.0.1",
+    author="mathrithms",
+    author_email="hello@mathrithms.com",
+    description="Hexadecimal Field for Django",
+    url="https://github.com/mathrithms/Django-Hexadecimal-Field.git",
     packages=['djangoHexadecimal'],
-    version='0.0.1',
-    license='MIT',
-    description='Hexadecimal Field for Django',
-    author='mathrithms',
-    author_email='hello@mathrithms.com',
-    url='https://github.com/mathrithms/Django-Hexadecimal-Field',   # Provide the link to your github
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
